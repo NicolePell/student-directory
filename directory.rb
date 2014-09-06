19edf405a8db3fc7529bbf8b95fe4030dbea24db
Nicole <pellicena@gmail.com>
2014-09-06 14:17:04 +0100

    ask user to input student names

--- directory.rb
+++ directory.rb
@@ -1,50 +1,42 @@
-# put the list of potential students into an array
-students = [
-	{:name => "vic329", :cohort => :september},
-	{:name => "Bernard", :cohort => :september},
-	{:name => "Elena Garrone", :cohort => :september},
-	{:name => "Yvette Cook", :cohort => :september},
-	{:name => "Stephen Giles", :cohort => :september},
-	{:name => "Ana Isabel Nogal", :cohort => :september},
-	{:name => "craigh44", :cohort => :september},
-	{:name => "Tim Scully", :cohort => :september},
-	{:name => "Ella Schofield", :cohort => :september},
-	{:name => "Fadie H", :cohort => :september},
-	{:name => "Colin Schmid aka mala23", :cohort => :september},
-	{:name => "zrasool88", :cohort => :september},
-	{:name => "galicians", :cohort => :september},
-	{:name => "danjocutler", :cohort => :september},
-	{:name => "Camilla van Klinken", :cohort => :september},
-	{:name => "Sandrine", :cohort => :september},
-	{:name => "jamesascarter", :cohort => :september},
-	{:name => "sistevens", :cohort => :september},
-	{:name => "Schlap", :cohort => :september},
-	{:name => "andrewhercules", :cohort => :september},
-	{:name => "Karin Nielsen", :cohort => :september},
-	{:name => "Andrew Snead", :cohort => :september},
-	{:name => "Rachel Nolan", :cohort => :september},
-	{:name => "galalag", :cohort => :september},
-	{:name => "annaschechter", :cohort => :september},
-	{:name => "alexfakhri", :cohort => :september},
-	{:name => "Denise Yu", :cohort => :september},
-	{:name => "Nicole Pellicena Tredway (ME)", :cohort => :september}
-]
 # create a method that prints the header
 def print_header
 	puts "The students in my cohort (September 2014) at Makers Academy"
 	puts "----------"
 end
+
 # create a method that prints the list of students
 def print(students)
 	students.each do |student|
 		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
+
 # create a method that prints the total number of students
 def print_footer(names)
 	puts "Overall, we have #{names.length} potential students."
 end
+
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	# create an empty array
+	students = []
+	# get the first name
+	name = gets.chomp
+	# while the name is not empty, repeat this code
+	while !name.empty? do
+		# add the student hash to the array
+		students << {:name => name, :cohort => :september}
+		puts "Now we have #{students.length} students."
+		# get another name from the user
+		name = gets.chomp
+	end
+	# return the array of students
+	students
+end
+
 # call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
